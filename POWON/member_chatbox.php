0e12cfaa6a43426d82c5e1393e9912389f10ad31
snafo <qinyuanwen@hotmail.com>
2016-08-06 22:05:08 -0400

    add chat box

--- POWON/member_chatbox.php
+++ POWON/member_chatbox.php
@@ -5,8 +5,8 @@
  * Date: 31/07/2016
  * Time: 5:05 PM
  */
+include './common/common.php';
 
-include 'logincheck.php';
 
 if(empty($_COOKIE['uid']))
 {
@@ -20,16 +20,33 @@ if(empty($_COOKIE['uid']))
 
 $uid = $_GET['uid'];
 
-if ($uid == $_COOKIE['uid']){
-    header('location:home.php');
+
+if($_POST['msgsubmitbtn']) {
+    $msg = $_POST['msg'];
+    if($msg!=''){
+        $result = dbInsert('chat','uid,fid,msg,posttime', ''.$_COOKIE['uid'].','.$uid.',"'.$msg.'",'.time().'');
+    }
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+
 }
+//
+//    $result = dbInsert('chat','uid,fid,msg,posttime', ''.$_COOKIE['uid'].','.$uid.',"'.$msg.'",'.time().'');
+//
+//    $history = dbSelect('chat','*','(uid='.$_COOKIE['uid'].' AND fid='.$uid.') OR (fid='.$_COOKIE['uid'].' AND uid='.$uid.')','posttime desc','');
+//
+//    $sender = dbSelect('user','uid,username,picture','uid='.$_COOKIE['uid'].'','','');
+//    $receiver = dbSelect('user','uid,username,picture','uid='.$uid.'','','');
+//
+//
+//if($history){
+//        foreach ($history as $row){
+//            echo "<span class='uid'>".$row['uid']."</span>: <span class='msg'>".$row['msg']."</span></br>";
+//            echo gmdate("Y-m-d\TH:i:s\Z ", $row['posttime'])."</br>";
+//        }
+//    }
+//    else{
+//        echo "Loading chatlogs please wait...";
+//    }
+//
 
 
-$uname = $_REQUEST['uid'];
-$msg = $_REQUEST['msg'];
-
-
-
-
-
-include template("member_chatbox.html");
